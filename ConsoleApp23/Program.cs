d0642401eebc49d2954624c9fccdd83a1d87de4d
Solehzoda <salikhovajillo@gmail.com>
2024-12-22 12:26:19 +0500

    first commit

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    { 
+        static void chek(int a)
+        {
+            var d = a;
+            if (d % 2 == 0)
+            {
+                Console.WriteLine("Чётное");
+            }
+            else
+            {
+                Console.WriteLine("Не чётное");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            chek(1);
+            chek(2);
+        }
+    }
+}      
+    
